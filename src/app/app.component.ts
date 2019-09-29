e873bd40de3c681daac6447495ec9794b466a671
jvw <jvw91@msn.com>
2019-09-29 18:04:18 +0200

    adding meta tags and site title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Title, Meta} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nixfornix';
+
+  constructor(private titleService: Title, private meta: Meta) {
+    titleService.setTitle('NixforNix - Urenregistratie');
+    meta.addTag({name: 'description', content: ''}, true);
+    meta.addTag({name: 'viewport', content: 'width=device-width, initial-scale=1'}, true);
+    meta.addTag({name: 'robots', content: 'all,follow'}, true);
+  }
 }
