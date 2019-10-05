80d8156cbe1396f30c564a6039e6439f17aa6c6b
jvw <jvw91@msn.com>
2019-10-05 16:17:59 +0200

    Added form reset after submit. Added default selected breaktime. (request by client) Added default selected Location. (request by client) Changed break boolean to time. (request by client)

--- src/app/web/hour-form/hour-form.component.ts
+++ src/app/web/hour-form/hour-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TimeRow} from '../../shared/models/timeRow.model';
 
 @Component({
@@ -7,18 +7,41 @@ import {TimeRow} from '../../shared/models/timeRow.model';
   styleUrls: ['./hour-form.component.css']
 })
 export class HourFormComponent implements OnInit {
-
   time: TimeRow = new TimeRow();
-
   times: Array<TimeRow> = [];
+
+  breaktimes = [0, 15, 30, 45, 60];
+  defaultBreakTime = 30;
+  defaultLocation = 'Luca Catering'; // TODO: Change to numeric value
+
+  getBreakName(num: number) {
+    if (num === 0) {
+      return 'Geen';
+    } else {
+      return String(num) + ' Minuten';
+    }
+  }
 
   onRegisterTime() {
     this.times.push(this.time);
+    this.resetTimeObj();
   }
 
-  constructor() { }
+  resetTimeObj() {
+    this.time.date = null;
+    this.time.startTime = null;
+    this.time.stopTime = null;
+    this.time.break = String(this.defaultBreakTime);
+    this.time.location = this.defaultLocation;
+    this.time.description = null;
+  }
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.time.break = String(this.defaultBreakTime);
+    this.time.location = this.defaultLocation;
   }
 
 }
