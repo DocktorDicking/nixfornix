fbe344fe465dd863231406ee9716a9c4109d2243
jvw <jvw91@msn.com>
2020-03-16 14:06:58 +0100

    Form validation for hour form
    hour-form.component.css:
    - Added css for error msges
    
    hour-form.component.html:
    - Changed form to support angular form lib
    
    hour-form.component.ts:
    - Added some logic to test input

--- src/app/web/hour-form/hour-form.component.ts
+++ src/app/web/hour-form/hour-form.component.ts
@@ -11,10 +11,17 @@ import {TimeService} from '../../shared/services/time.service';
 export class HourFormComponent implements OnInit {
   private time: TimeRow;
   breaktimes = [0, 15, 30, 45, 60];
+  message: string;
 
   submitTime() {
-    this.timeService.onRegisterTime(this.time);
-    this.time = this.timeService.newTimeObj();
+    if (this.timeFormValidation(this.time)) {
+      this.timeService.onRegisterTime(this.time);
+      this.time = this.timeService.newTimeObj();
+    } else {
+      setTimeout(() => {
+        this.message = undefined;  // TODO: Add directive to error msges and fadeout after 4 seconds.
+      }, 5000);
+    }
   }
 
   getBreakName(num: number) {
@@ -23,6 +30,35 @@ export class HourFormComponent implements OnInit {
     } else {
       return String(num) + ' Minuten';
     }
+  }
+
+  timeFormValidation(time: TimeRow): boolean {
+    if (!time) {
+      this.message = 'Tijd object is leeg. Kan dit formulier niet opslaan.'; // TODO: Something more sensible for the user?
+      return false;
+    }
+
+    const date = new Date(time.date);
+    if (!(date.getTime() === date.getTime())) {
+      this.message = 'Datum is niet valide of is leeg.';
+      return false;
+    }
+
+    if (!time.startTime) {
+      this.message = 'Begintijd is een verplicht veld. ';
+      return false;
+    }
+
+    if (!time.stopTime) {
+      this.message = 'Eindtijd is een verplicht veld. ';
+      return false;
+    }
+
+    if (time.startTime === time.stopTime) {
+      this.message = 'Pardon.. je heb 24 uur gewerkt?'; // TODO: Change to something nice
+      return false;
+    }
+    return true;
   }
 
   constructor(private timeService: TimeService) {
