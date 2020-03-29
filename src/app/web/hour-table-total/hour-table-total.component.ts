52d6a03a6d6537ff12242ea78d6a5424b6997064
jvw <jvw91@msn.com>
2020-03-29 14:49:43 +0200

    New component init for admin removed some derps

---
+++ src/app/web/hour-table-total/hour-table-total.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hour-table-total',
+  templateUrl: './hour-table-total.component.html',
+  styleUrls: ['./hour-table-total.component.css']
+})
+export class HourTableTotalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
