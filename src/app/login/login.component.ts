6547556da542ebc59bc4ea911c21dfd475f5a589
jvw <jvw91@msn.com>
2020-04-01 21:33:48 +0200

    Small changes
    login.component.ts
    Moved some more stuff to session service
    
    login.component.html
    Fixed error messages

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../shared/models/user.model';
+import { SessionService } from '../shared/services/session.service';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +9,6 @@ import { User } from '../shared/models/user.model';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  users = {
-    jim: 'welkom01',
-    nico: 'welkom02',
-    admin: 'admin'
-  };
-
   canLogin: boolean;
   message: string;
   user = new User(null);
@@ -22,38 +17,23 @@ export class LoginComponent implements OnInit {
    * Will submit data from login form and check if credentials exist and match.
    */
   onSubmit() {
-    this.canLogin = this.login(this.user);
+    this.canLogin = this.sessionService.login(this.user);
     if (this.canLogin) {
       this.router.navigate(['./home']);
-    } else if (this.message) {
-      setTimeout(() => {
-        this.message = undefined;  // TODO: Add directive to error msges and fadeout after 4 seconds.
-      }, 5000);
+    } else {
+      this.setMessage('Gebruikersnaam of wachtwoord incorrect.');
     }
   }
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private sessionService: SessionService) { }
 
   ngOnInit() {
   }
 
-  /**
-   * This method need to be moved the the login service sometime.
-   * @param user
-   */
-  private login(user: User) {
-    if (!user.hasCredentials()) {
-      return false;
-    }
-
-    // TODO: move to login service when backend haves database.
-    const data = this.users;
-    // @ts-ignore
-    if (!user.username in data || !(data[user.username] === user.password)) {
-      this.message = 'Gebruikersnaam of wachtwoord incorrect.'
-      return false;
-    } else {
-      return true;
-    }
+  private setMessage(message: string) {
+    this.message = message;
+    setTimeout(() => {
+      this.message = undefined;  // TODO: Add directive to error msges and fadeout after 4 seconds.
+    }, 5000);
   }
 }
