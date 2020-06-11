bf9685e730a8247a6b37808322bc292a6c08082b
jvw <jvw91@msn.com>
2020-06-12 01:28:27 +0200

    Same as with the user model. Privates in Typscript are a pain in the arse.

--- src/app/shared/models/timeRow.model.ts
+++ src/app/shared/models/timeRow.model.ts
@@ -2,14 +2,14 @@
  * Datamodel for timeRow. Used for hour registration and reading.
  */
 export class TimeRow {
-  private _id: number;
-  private _date: string;
-  private _startTime: string;
-  private _stopTime: string;
-  private _break: number;
-  private _location: string;
-  private _description: string;
-  private _hours: number;
+  id: number;
+  date: string;
+  startTime: string;
+  stopTime: string;
+  break: number;
+  location: string;
+  description: string;
+  hours: number;
 
   constructor() {}
 
@@ -18,75 +18,11 @@ export class TimeRow {
    * @param time TimeRow
    */
   cloneTimeRow(time: TimeRow) {
-    this._date = time.date;
-    this._startTime = time.startTime;
-    this._stopTime = time.stopTime;
-    this._break = time.break;
-    this._location = time.location;
-    this._description = time.description;
-  }
-
-  set id(value: number) { // TODO: Make return boolean to check if value was correct? Does Angular have a build in mechanism for this?
-    this._id = value;
-  }
-
-  set date(value: string) {
-    this._date = value;
-  }
-
-  set startTime(value: string) {
-    this._startTime = value;
-  }
-
-  set stopTime(value: string) {
-    this._stopTime = value;
-  }
-
-  set break(value: number) {
-    this._break = value;
-  }
-
-  set location(value: string) {
-    this._location = value;
-  }
-
-  set description(value: string) {
-    this._description = value;
-  }
-
-  set hours(value: number) {
-    this._hours = value;
-  }
-
-  get id(): number {
-    return this._id;
-  }
-
-  get date(): string {
-    return this._date;
-  }
-
-  get startTime(): string {
-    return this._startTime;
-  }
-
-  get stopTime(): string {
-    return this._stopTime;
-  }
-
-  get break(): number {
-    return this._break;
-  }
-
-  get location(): string {
-    return this._location;
-  }
-
-  get description(): string {
-    return this._description;
-  }
-
-  get hours(): number {
-    return this._hours;
+    this.date = time.date;
+    this.startTime = time.startTime;
+    this.stopTime = time.stopTime;
+    this.break = time.break;
+    this.location = time.location;
+    this.description = time.description;
   }
 }
