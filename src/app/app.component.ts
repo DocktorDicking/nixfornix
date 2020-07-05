501f6e83faebbf829e527339d678381c3261d595
jvw <jvw91@msn.com>
2020-07-05 17:08:21 +0200

    Removed timeservice from constructor since it is nog needed here.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
   title = 'nixfornix';
 
   // TODO: Check if TimeService actually need to be initialized in this constructor. Seems to me its bollocks.
-  constructor(private titleService: Title, private meta: Meta, private timeService: TimeService) {
+  constructor(private titleService: Title, private meta: Meta) {
     titleService.setTitle('NixforNix - Urenregistratie');
     meta.addTag({name: 'description', content: ''}, true);
     meta.addTag({name: 'viewport', content: 'width=device-width, initial-scale=1'}, true);
