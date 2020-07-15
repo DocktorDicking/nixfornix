9bb489e505757571a8a33c9f8a844176e42a7f9e
jvw <jvw91@msn.com>
2020-07-15 09:07:17 +0200

    Working on authentication

--- src/app/shared/services/session.service.ts
+++ src/app/shared/services/session.service.ts
@@ -10,36 +10,103 @@ export class SessionService {
   // TODO: Plan for now is to replace this with authService
 
   // Tmp array with user objects
-  private users = {
+  private DATA_SOURCE = {
     jim: 'welkom01',
     nico: 'welkom02',
     admin: 'admin',
     john: 'welkom'
   };
 
+  /**
+   * Will authenticate a user based on pw and email. Will set a localstorage item to remember pw/email.
+   * The localstorage item will be valid for 30 days. After 30 days the user need to reauthenticate.
+   * @param user User
+   */
   public login(user: User) {
     if (!user.hasCredentials()) {
       return false;
     }
 
-    user.admin = (user.email === 'admin');
-    const data = this.users; // TODO: Replace this when we have a db.
-    if (user.email in data && (data[user.email] === user.password)) {
+    if (localStorage.length > 0) {
+      const authEmail = localStorage.getItem('auth_email');
+      const authPassword = localStorage.getItem('auth_password');
+      const authDate = localStorage.getItem('auth_date');
+      if (authEmail && authPassword && authDate) {
+        // TODO: Replace DATA_SOURCE with API call.
+        if (this.dateDiffInDays(new Date(authDate), new Date()) < 30) {
+          if (authEmail in this.DATA_SOURCE && (this.DATA_SOURCE[authEmail] === authPassword)) {
+            sessionStorage.setItem('auth_email', authEmail);
+            sessionStorage.setItem('auth_password', authPassword);
+            return true;
+          }
+        } else {
+          localStorage.removeItem('auth_email');
+          localStorage.removeItem('auth_password');
+          localStorage.removeItem('auth_date');
+        }
+      }
+    }
+
+    // TODO: Replace DATA_SOURCE with API call.
+    if (user.email in this.DATA_SOURCE && (this.DATA_SOURCE[user.email] === user.password)) {
       sessionStorage.setItem('auth_email', user.email);
       sessionStorage.setItem('auth_password', user.password);
-      sessionStorage.setItem('auth_date', new Date().toISOString());
+      user.admin = (user.email === 'admin'); // TODO: Remove before prod.
+
+      // TODO: Stay logged in checkbox check here. (auth_auto)
+      if (true) {
+        localStorage.setItem('auth_date', new Date().toISOString());
+        localStorage.setItem('auth_email', user.email);
+        localStorage.setItem('auth_password', user.password);
+      }
       return true;
     }
   }
 
+  /**
+   * Checks if a user is authenticated by checking session for auth variables.
+   * If auth variables exist in session storage they will be evaluated.
+   * @param user User
+   */
+  public isAuthenticated(user: User) {
+    if (sessionStorage.length > 0) {
+      const authEmail = sessionStorage.getItem('auth_email');
+      const authPassword = sessionStorage.getItem('auth_password');
+      if (authEmail && authPassword) {
+        // TODO: Check if pw/email exist in db
+        if (authEmail === user.email && authPassword === user.password) {
+          return (authEmail in this.DATA_SOURCE && (this.DATA_SOURCE[authEmail] === authPassword));
+        } else {
+          sessionStorage.removeItem('auth_password');
+          sessionStorage.removeItem('auth_email');
+        }
+        return false;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Will delete auth variables in sessionStorage and localStorage.
+   */
   public logout() {
     if (sessionStorage.length > 0) {
       sessionStorage.removeItem('auth_email');
       sessionStorage.removeItem('auth_password');
-      sessionStorage.removeItem('auth_date');
+    }
+    if (localStorage.length > 0) {
+      localStorage.removeItem('auth_email');
+      localStorage.removeItem('auth_password');
+      localStorage.removeItem('auth_date');
     }
   }
 
+  /**
+   * Returns difference between two dates in days.
+   * Will use date 'a' as main.
+   * @param a Date
+   * @param b Date
+   */
   public dateDiffInDays(a: Date, b: Date) {
     const MS_PER_DAY = 1000 * 60 * 60 * 24;
     const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
