9e3c140aefe474b3f3a3b362cbff725a3902c9ab
jvw <jvw91@msn.com>
2021-10-22 16:52:22 +0200

    Working on active/non-active checking of users. Api will send error containing 'Unauthorized' when a user is set to inactive while the user is still logged into the system or haves a persistent login token. User will be logged out and redirected to the login page.
    User will get message when trying to log in when account is inactive.
    
    ErrorInterceptor.service.ts:
     - changed the onError method so it checks for Unauthorized in response.error.error

--- src/app/shared/services/interceptors/ErrorInterceptor.service.ts
+++ src/app/shared/services/interceptors/ErrorInterceptor.service.ts
@@ -4,6 +4,7 @@ import {Observable, throwError} from 'rxjs';
 import {catchError} from 'rxjs/operators';
 import {MessageService} from '../message.service';
 import {ToastrService} from 'ngx-toastr';
+import {AuthService} from '../auth.service';
 
 /**
  * Checks all HTTP requests on errors.
@@ -11,7 +12,7 @@ import {ToastrService} from 'ngx-toastr';
 @Injectable()
 export class ErrorInterceptorService implements HttpInterceptor {
 
-  constructor(private messageService: MessageService, private toastr: ToastrService) {}
+  constructor(private messageService: MessageService, private toastr: ToastrService, private auth: AuthService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next
@@ -28,11 +29,17 @@ export class ErrorInterceptorService implements HttpInterceptor {
    * Sets message in message service so it can be send to the component currently active.
    */
   private onError(response: HttpErrorResponse): void {
-    const apiMessage = response.error.message;
-    const message = this.getMessage(apiMessage);
-    if (message) {
-      // TODO we are going to change all messaging to toastr, so we need to replace the whole message service and nuke it.
-      this.toastr.error(message, this.getTitle(apiMessage));
+    // TODO we are going to change all messaging to toastr, so we need to replace the whole message service and nuke it.
+    if (response.error.error === 'Unauthorized') {
+      this.auth.logout();
+      this.toastr.error(this.getMessage('Unauthorized'), this.getTitle('Unauthorized'));
+    } else {
+      const apiMessage = response.error.message;
+      const message = this.getMessage(apiMessage);
+      if (message) {
+        this.toastr.error(message, this.getTitle(apiMessage));
+      }
+
       // this.messageService.changeMessage(message);
     }
   }
@@ -44,6 +51,10 @@ export class ErrorInterceptorService implements HttpInterceptor {
     switch (apiMessage) {
       case 'Bad credentials':
         return 'Gebruikersnaam of wachtwoord incorrect.';
+      case 'User is disabled':
+        return 'Inloggen is niet mogelijk. Dit account is niet actief.';
+      case 'Unauthorized':
+        return 'Je heb geen authorisatie om deze handeling uit te voeren. Je bent uitgelogd. Probeer opnieuw in te loggen.';
       default:
         return apiMessage;
     }
@@ -56,6 +67,10 @@ export class ErrorInterceptorService implements HttpInterceptor {
     switch (apiMessage) {
       case 'Bad credentials':
         return 'Inloggen mislukt.';
+      case 'User is disabled':
+        return 'Account non-actief';
+      case 'Unauthorized':
+        return 'Geen authorisatie';
       default:
         return apiMessage;
     }
