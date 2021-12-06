144637bb93f6b54cd07a1a7a540b7da429e7663e
jvw <jvw91@msn.com>
2021-12-06 16:21:14 +0100

    Update 0.9.2 Working on profile component and formfooter
    Added new global css class 'formfooter' to streamline the button styling across the whole app.
    Added new global css class 'btn-secondary' to make the button color less hard-ish
    
    Profile is still a work in progress.

--- src/app/web/usercomponents/profile/profile.component.ts
+++ src/app/web/usercomponents/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../../shared/models/user.model';
+import {AuthService} from '../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  public user: User;
+  public hidePw = true;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.user = this.authService.currentUserValue;
   }
 
+  toggleShowPw() {
+    this.hidePw = !this.hidePw;
+  }
+
+  submitProfile() {
+    // TODO update call
+  }
+
+  changePassword() {
+    // TODO update password
+  }
 }
