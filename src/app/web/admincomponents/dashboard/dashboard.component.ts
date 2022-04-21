ecfceeb33a2f6a93b7c367c7bebac5511f4c9eef
Jim <jvw91@msn.com>
2022-04-21 21:14:29 +0200

    Update v0.9.7.0: Working on dashboard
    We are making 2 charts on the same dataset.

--- src/app/web/admincomponents/dashboard/dashboard.component.ts
+++ src/app/web/admincomponents/dashboard/dashboard.component.ts
@@ -7,18 +7,21 @@ import {ChartType} from 'chart.js';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  public barChartOptions = {
+  public chartOptions = {
     scaleShowVerticalLines: false,
     responsive: true
   };
-
-  public barChartLabels = ['jan', 'feb', 'mrt', 'apr', 'mei', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'dec'];
-  public barChartType: ChartType = 'bar';
-  public barChartLegend = true;
-  public barChartData = [
+  public chartLabels = ['jan', 'feb', 'mrt', 'apr', 'mei', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'dec'];
+  public chartLegend = true;
+  public chartData = [
     {data: [65, 59, 80, 81, 56, 55, 40, 25, 45, 80, 98, 120], label: '2021'},
     {data: [28, 48, 40, 19, 86, 27, 90, 30, 55, 60, 40, 160], label: '2022'}
   ];
+
+  public barChartType: ChartType = 'bar';
+  public lineChartType: ChartType = 'line';
+
+
 
   constructor() { }
 
