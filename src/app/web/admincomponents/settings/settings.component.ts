4d8835ee6fd4e22f7841e950b992256dc7cb2f85
Jim <jvw91@msn.com>
2023-05-10 21:40:30 +0200

    Update v0.9.8: Ticket #16 Implementing the setting component by adding content to the html and ts file.
    ~ settings.component.html:
    Adding the required fields and most of the logic for the html.
    
    ~ settings.component.ts:
    Adding the logic required for the html.

--- src/app/web/admincomponents/settings/settings.component.ts
+++ src/app/web/admincomponents/settings/settings.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {SettingModel} from '../../../shared/models/setting.model';
+import {SettingService} from '../../../shared/services/setting.service';
+import {LocationService} from '../../../shared/services/location.service';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +10,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  private settingData: SettingModel[];
 
-  constructor() { }
+  public logo = '';
+  public defaultBreaktime: number;
+  public breakTimes: number[];
+  public defaultLocation: string;
+  public adminRegisterTime: boolean;
+  public adminManageUsers: boolean;
+  public maxActiveUsers: number;
+
+  constructor(private route: ActivatedRoute, private locationService: LocationService) {
+    this.locationService.getLocations();
+
+    // Reads the settingData from the RouteResolver, see SettingDataResolver.
+    this.route.data.subscribe(() => {
+      this.settingData = this.route.snapshot.data.settingData;
+    });
+  }
 
   ngOnInit() {
+    // Initializing setting vars.
+    this.settingData.forEach((setting) => {
+      switch (setting.name) {
+        case SettingService.LOGO:
+          this.logo = setting.value;
+          break;
+        case SettingService.DEFAULT_BREAKTIME:
+          this.defaultBreaktime = JSON.parse(setting.value);
+          break;
+        case SettingService.BREAKTIMES:
+          this.breakTimes = JSON.parse(setting.value);
+          break;
+        case SettingService.DEFAULT_LOCATION:
+          this.defaultLocation = setting.value;
+          break;
+        case SettingService.ADMIN_REGISTER_TIME:
+          this.adminRegisterTime = JSON.parse(setting.value);
+          break;
+        case SettingService.ADMIN_MANAGE_USERS:
+          this.adminManageUsers = JSON.parse(setting.value);
+          break;
+        case SettingService.MAX_USERS:
+          this.maxActiveUsers = JSON.parse(setting.value);
+          break;
+      }
+    });
   }
 
 }
