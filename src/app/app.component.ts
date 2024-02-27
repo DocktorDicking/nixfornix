f297be48da69b20b569a1bd7aabd433edf92fac3
Jim <jvw91@msn.com>
2024-02-27 14:09:34 +0100

    UPDATE v0.9.9: Update robots meta tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import {Meta, Title} from '@angular/platform-browser';
 import {TimeService} from './shared/services/time.service';
 import {StateService} from './shared/services/state.service';
 import {UserService} from './shared/services/user.service';
-import {AuthService} from './shared/services/auth.service';
-import {HttpClient} from '@angular/common/http';
 import {ExcelService} from './shared/services/excel.service';
 import {MailerService} from './shared/services/mailer.service';
 import {ChartdataService} from './shared/services/chartdata.service';
@@ -15,15 +13,16 @@ import {LocationService} from './shared/services/location.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  // Services delcared here are considered global and
   providers: [TimeService, StateService, UserService, ExcelService, MailerService, ChartdataService, AdminLogService, LocationService]
 })
 export class AppComponent {
   title = 'nix4nix';
 
-  constructor(private titleService: Title, private meta: Meta, private http: HttpClient, private auth: AuthService) {
+  constructor(private titleService: Title, private meta: Meta) {
     titleService.setTitle('Nix4Nix - Urenregistratie');
     meta.addTag({name: 'description', content: ''}, true);
     meta.addTag({name: 'viewport', content: 'width=device-width, initial-scale=1'}, true);
-    meta.addTag({name: 'robots', content: 'all,follow'}, true);
+    meta.addTag({name: 'robots', content: 'noindex'}, true);
   }
 }
